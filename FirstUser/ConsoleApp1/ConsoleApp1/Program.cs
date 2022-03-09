b730dff53072c5fcda1250f3207960f9f455985f
Emanuel Caprariu <manu.caprariu.brediceanu@gmail.com>
2022-03-09 15:48:58 +0200

    second user for conflict

--- FirstUser/ConsoleApp1/ConsoleApp1/Program.cs
+++ FirstUser/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 Console.WriteLine("Dati ceva");
 String a = Console.ReadLine();
 Console.WriteLine("First user in development branch");
+Console.WriteLine("TRALAALALALA AL LAALALAL FIRST USER CHANGES");
 try
 {
     
